401a35880cc6844c7c77101666698c4bbadf84ae
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-05-27 15:48:35 -0300

    css btn_neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Btn_Neon } from "@/components/btn/btn_neon/Btn_Neon";
 
 export default function Home() {
   return (
     <main>
-     
+     <Btn_Neon/>
     </main>
   );
 }
