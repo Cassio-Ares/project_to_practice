b7c9999fc2b7de77bc38bed2a392b874b33e9812
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-05-29 18:13:58 -0300

    styled css

---
+++ src/components/btn_toggler/Toggle_On_Off.tsx
@@ -0,0 +1,20 @@
+import './styled.css'
+import './style.scss'
+
+export const Toggle_On_Off = () => {
+  return (
+    <div className='container_toggle'>
+      <label htmlFor='toggle' className='label_toggle'>
+        <input type='checkbox' id='toggle' className='check'/>
+        <div className='slider slider--0'>ON</div>
+        <div className='slider slider--1'>
+            <div></div>
+            <div></div>
+        </div>
+        <div className='slider slider--2'></div>
+        <div className='slider slider--3'>OFF</div>
+      </label>
+    </div>
+  );
+}
+
--- src/components/view/HomeViews.tsx
+++ src/components/view/HomeViews.tsx
@@ -5,23 +5,27 @@ import Grid from '@mui/material/Grid';
 import { Btn_Neon } from '@/components/btn/btn_neon/Btn_Neon';
 import { Btn_ConfirmV } from '@/components/btn/btn_confirmV/Btn_ConfirmV';
 import { CardHover } from '../cardHover/CardHover';
+import { Toggle_On_Off } from '../btn_toggler/Toggle_On_Off';
 
 
 
-export default function HomeViews () {
+export default function HomeViews() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={{ xs: 3, md: 10 }} columns={{ xs: 4, sm: 10, md: 12 }}>
-          <Grid item xs={2} sm={4} md={4}>
-            <Btn_Neon/>
-          </Grid>
-          <Grid item xs={2} sm={4} md={4}>
-            <CardHover/>
-          </Grid>
-          <Grid item xs={2} sm={4} md={4}>
-            <Btn_ConfirmV/>
-          </Grid>
-         
+        <Grid item xs={2} sm={4} md={4}>
+          <Btn_Neon />
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <CardHover />
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Btn_ConfirmV />
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <Toggle_On_Off />
+        </Grid>
+
       </Grid>
     </Box>
   );
