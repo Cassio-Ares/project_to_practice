1814cfc0ad6556d873f90c57d1db381c82c4f161
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-05-31 19:14:15 -0300

    ajuste de posição

--- src/components/view/HomeViews.tsx
+++ src/components/view/HomeViews.tsx
@@ -28,24 +28,30 @@ export default function HomeViews() {
 
       <Box_Container sx={{ flexGrow: 1 }}>
         <Grid container spacing={{ xs: 3, md: 10 }} columns={{ xs: 4, sm: 10, md: 12 }}>
-          {/* <Grid item xs={2} sm={4} md={4}>
+           <Grid item xs={2} sm={4} md={4}>
             <Btn_Neon />
           </Grid>
+
           <Grid item xs={2} sm={4} md={4}>
             <CardHover />
           </Grid>
+
           <Grid item xs={2} sm={4} md={4}>
             <Btn_ConfirmV />
           </Grid>
+
           <Grid item xs={2} sm={4} md={4}>
             <Toggle_On_Off />
           </Grid>
+
           <Grid item xs={2} sm={4} md={4}>
             <Nav_Link_Contact />
-          </Grid> */}
+          </Grid> 
+
           <Grid item xs={2} sm={4} md={4}>
             <Slice_Transition />
           </Grid>
+          
         </Grid>
       </Box_Container>
     </main>
