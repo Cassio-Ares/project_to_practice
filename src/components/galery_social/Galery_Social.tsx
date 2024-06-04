ed49c39114d93f43db7abac34abf317eb8f1bc51
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-06-04 19:03:23 -0300

    galery social

--- src/components/galery_social/Galery_Social.tsx
+++ src/components/galery_social/Galery_Social.tsx
@@ -17,7 +17,7 @@ export const  Galery_Social = () => {
     return (
         <div className='container'>
             <div className="profile" onClick={handleProfileClick}>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ02mof1BT5P7qOliU3-5MFdlN59V6yzB6vnw&s" alt="" />
+                <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhF8INWtLSA-2t8kI9RvSoDUSkLRIphCrvJ7jsAz_nQaUXNqRpE8sHbUVcTRW72O8H2KBwWleZf4zNY7UR4LFnDEInlFiTn3HruAOZMOJRE5rQH6kEQEiT1J2ttMOgZLgSmMtx33oKJNCs/s320/CodeGeass_Lelouch1.jpg" alt="" />
                 <div className="overlay"></div>
                 <div className="plus"></div>
             </div>
@@ -27,7 +27,7 @@ export const  Galery_Social = () => {
                 <div className="plus"></div>
             </div>
             <div className="profile" id='profile-3'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ02mof1BT5P7qOliU3-5MFdlN59V6yzB6vnw&s" alt="" />
+                <img src="https://pm1.aminoapps.com/6476/b97b7b8157934c37f5cd3e7a57100e1e9cec7469_hq.jpg" alt="" />
                 <div className="overlay"></div>
                 <div className="plus"></div>
             </div>
@@ -37,16 +37,17 @@ export const  Galery_Social = () => {
                 <div className="plus"></div>
             </div>
 
-            <div className={`detail ${isDetailActive ? 'active' : ''}`} id="detail_1">
+            <div className={`detail ${isDetailActive ? 'active' : ''}`} id="detail_1" >
                 <div className="close" onClick={handleCloseClick}></div>
+               
                 <img src="https://static.preparaenem.com/2022/08/paisagem-natural-suica.jpg" alt="" className="header" />
 
                 <div className='image'>
-                    <img src='https://100dayscss.com/codepen/13-1.jpg' alt='' />
+                    <img src='https://i.pinimg.com/564x/4b/b3/e2/4bb3e23730ce021044ea66eeaf85a401.jpg' alt='' />
                 </div>
 
                 <div className="infos">
-                    <div className="name">Cassio</div>
+                    <div className="name">Lelouch Vi Britannia</div>
                     <div className="action">
                         <div className='btn'>
                             <span className='fa fa-phone'><FaPhone/></span>
