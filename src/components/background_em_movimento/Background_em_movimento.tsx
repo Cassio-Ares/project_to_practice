aec6dcf74a3046a116ea6dc8a41ff79ec298ed3c
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-06-07 17:21:44 -0300

    css

---
+++ src/components/background_em_movimento/Background_em_movimento.tsx
@@ -0,0 +1,10 @@
+import './stylesBK.css';
+
+export const Background_em_movimento = () => {
+  return (
+    <div className='container_background'>
+        <div className='bk-1'></div>
+        <div className='bk-2'></div>
+    </div>
+  )
+}
--- src/components/view/HomeViews.tsx
+++ src/components/view/HomeViews.tsx
@@ -17,6 +17,7 @@ import { Troca_com_styled_component } from "../troca_de_estilo_c_click/troca_com
 import { Neomorphism_Animation } from "../neomorphism_animation/Neomorphism_Animation";
 import { Carrossel_animation } from "../carrossel_animation/Carrossel_animation";
 import { Elemento_seguindo_mouse } from "../ponto_que_segue_mouse/Elemento_seguindo_mouse";
+import { Background_em_movimento } from "../background_em_movimento/Background_em_movimento";
 
 
 export default function HomeViews() {
@@ -33,7 +34,7 @@ export default function HomeViews() {
 
       <Box_Container sx={{ flexGrow: 1 }}>
         <Grid container spacing={{ xs: 3, md: 10 }} columns={{ xs: 4, sm: 10, md: 12 }}>
-          <Grid item xs={2} sm={4} md={4}>
+          {/* <Grid item xs={2} sm={4} md={4}>
             <Btn_Neon />
           </Grid>
 
@@ -78,6 +79,10 @@ export default function HomeViews() {
 
           <Grid item xs={2} sm={4} md={4}>
             <Elemento_seguindo_mouse />
+          </Grid> */}
+
+          <Grid item xs={2} sm={4} md={4}>
+            <Background_em_movimento />
           </Grid>
 
         </Grid>
